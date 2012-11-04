9d87fdb8fed5208dc5ac3a20c1d440f014c0927a
tokuhirom <tokuhirom@gmail.com>
2012-11-05 02:07:45 +0900

    initial import

---
+++ lib/Text/TestBase.pm
@@ -0,0 +1,39 @@
+package Text::TestBase;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Text::TestBase - ...
+
+=head1 SYNOPSIS
+
+  use Text::TestBase;
+
+=head1 DESCRIPTION
+
+Text::TestBase is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

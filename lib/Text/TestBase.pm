5873fbce5da3dafb173f39dcb1f6b29f72873b7c
tokuhirom <tokuhirom@gmail.com>
2012-11-06 19:01:27 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b113e17..64d6cad 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Text::TestBase
    
    +0.06
    +
    +    - added 'run_is' function and 'uc' filter
    
     0.05

--- lib/Text/TestBase.pm
+++ lib/Text/TestBase.pm
@@ -2,7 +2,7 @@ package Text::TestBase;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Class::Accessor::Lite (
     rw => [qw/block_delim data_delim block_class/],
